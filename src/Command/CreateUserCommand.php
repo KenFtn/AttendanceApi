3e5dcaaece32cd0adc64ec7b6212c60081875d08
MarieBonifacio <mariebonifacio.pro@gmail.com>
2020-05-25 17:23:47 +0200

    add command

---
+++ src/Command/CreateUserCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class CreateUserCommand extends Command
+{
+    
+    private $userPasswordEncoder;
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager, UserPasswordEncoderInterface $userPasswordEncoder)
+    {
+        $this->entityManager = $entityManager;
+        $this->userPasswordEncoder = $userPasswordEncoder;
+    }
+
+    protected function configure()
+    {
+        $this->setName('app:user-default');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $admin = new User();
+        $admin->setEmail('admin@admin.com');
+        $admin->setUsername('admin');
+        $admin->setFirstname('admin');
+        $admin->setLastname('admin');
+        $admin->setPassword($this->userPasswordEncoder->encodePassword($admin, 'admin'));
+        $this->entityManager->persist($admin);
+        $this->entityManager->flush();
+        return 0;
+    }
+}
+
+?>
